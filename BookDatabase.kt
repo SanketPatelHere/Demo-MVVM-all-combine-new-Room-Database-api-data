bd299461afd3b5ece5157c3567cc4a305e0908a4
sanket patel <51230342+SanketPatelHere@users.noreply.github.com>
2024-06-03 11:34:53 +0530

    Delete BookDatabase.kt

--- BookDatabase.kt
+++
@@ -1,8 +0,0 @@
-package com.demomvvm.MVVM.RoomDatabase
-
-import androidx.room.*
-
-@Database(entities = [Book::class], version = 1)
-abstract class BookDatabase : RoomDatabase() {
-    abstract fun bookDao(): BookDao
-}
